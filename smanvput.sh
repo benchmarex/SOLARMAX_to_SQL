2a29fc2801747eb6629214fff973b0a978724cf3
benchmarex <120053921+benchmarex@users.noreply.github.com>
2023-03-31 09:11:52 +0200

    SQl funcionality

--- smanvput.sh
+++ smanvput.sh
@@ -3,7 +3,7 @@
 # Simple script to run the Python code with personal parameters
 # replace the IDs and keys with yours below
 
-cd /home/pi/SOLARMAX_SOLAR_to_SQL
+cd /home/pi/SOLARMAX_to_SQL
 python3 main_solarmax_sql.py
 
 # END OF FILE
